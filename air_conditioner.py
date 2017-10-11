9118992bd3738ad0b5683a9de7837acdfb8d119e
Vinícius Fraga <vinicius.vmsf@gmail.com>
2017-10-11 18:52:01 -0300

    Updated air_conditioner with -s arg.

--- air_conditioner.py
+++ air_conditioner.py
@@ -1,42 +1,45 @@
-import paho.mqtt.client as mqtt
+from multiprocessing import Process
 from time import sleep
-import json
-import random
+import paho.mqtt.client as mqtt
 import argparse
 
-objective = 33
-temperature = 33
 
-def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-	client.subscribe("commands/air_conditioner")
+def air_conditioner(device_id):
+    objective = 33
+    temperature = 33
 
-def on_message(client, userdata, msg):
-	global objective
-	objective = int(msg.payload.decode())
+    def on_connect(client, userdata, flags, rc):
+        print("Connected with result code "+str(rc))
+        client.subscribe("commands/air_conditioner")
+
+    def on_message(client, userdata, msg):
+        nonlocal objective
+        objective = int(msg.payload.decode())
 
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+    client.connect("localhost", 1883, 60)
+    client.loop_start()
 
-client.loop_start()
+    while True:
+        sleep(1)
+        if temperature > objective:
+            temperature -=1
+        elif temperature < objective:
+            temperature += 1
+        client.publish("drivers/virtual_air", device_id+";"+str(temperature)+";")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("id",
-                    help="device id",
-                    type=str)
+    client.loop_stop()
 
-device_id = parser.parse_args().id
 
-while True:
-	sleep(2)
-	if temperature > objective:
-		temperature -=1
-	elif temperature < objective:
-		temperature += 1
-	client.publish("drivers/virtual_air", device_id+";"+str(temperature)+";")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s","--size", type=int,
+                        help="how many device instances to create", default=1)
+    args = parser.parse_args()
 
-client.loop_stop()
+    for device_id in range(args.size):
+        Process(target=air_conditioner, args=(str(device_id),)).start()
